3afc355f353cd5b39c8a6b7125bbbdc941383ddb
xiechanglei <xiechanglei@139.com>
2025-11-26 13:18:29 +0800

    gogogo

--- public/component/header.js
+++ public/component/header.js
@@ -20,9 +20,9 @@ class HeaderComponent extends HTMLElement {
                         <slot />
                     </div>
                     <div class="nav-right">
-                        <a href="/index.html" class="nav-link">首页</a>
+                        <a href="/index.html" class="nav-link site">首页</a>
+                        <a href="/pages/about.html" class="nav-link site">关于</a>
                         <a href="https://github.com/xiechanglei/mortal-blog" target="_blank" class="nav-link">GitHub</a>
-                        <a href="/pages/about.html" class="nav-link">关于</a>
                         <a href="https://music.163.com/#/playlist?id=14231616354" target="_blank" class="nav-link">歌单</a>
                     </div>
                 </nav>
@@ -33,7 +33,7 @@ class HeaderComponent extends HTMLElement {
 
         // 站内的网页，需要对nav进行过滤，比如当前页面是about.html，那么nav中关于about的链接需要高亮显示，表示当前页面，点击无效
         const currentPath = window.location.pathname;
-        const navLinks = this.shadowRoot.querySelectorAll('.nav-link');
+        const navLinks = this.shadowRoot.querySelectorAll('.nav-link.site');
         navLinks.forEach(link => {
             const linkPath = new URL(link.href).pathname;
             if (linkPath === currentPath || (currentPath + ".html") === linkPath) { // cloudflare pages会自动去掉.html后缀
