14d1a53135536146a95fcb63cee277c2fc9da012
xiechanglei <xiechanglei@139.com>
2025-11-28 11:53:02 +0800

    add icon

--- data/article-data.js
+++ data/article-data.js
@@ -4,7 +4,7 @@ const allArticles = [
         "filePath": "0001.2025-empty.md",
         "cover": "/covers/hello-world.jpg",
         "date": "2025-11-25",
-        "wordCount": 63,
+        "wordCount": 957,
         "preview": "道生一，一生二，二生三，三生万物。",
         "tags": [
             "随笔"
